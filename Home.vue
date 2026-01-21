8bffe2b14c98e57300490ba6bc4dddf202b772f9
GeorgyPylyp <132196813+GeorgyPylyp@users.noreply.github.com>
2026-01-21 22:16:12 +0200

    Create Home.vue

---
+++ Home.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="max-w-6xl mx-auto p-6">
+    <h1 class="text-3xl font-bold mb-4">Ласкаво просимо до Paint Shop</h1>
+    <p class="text-gray-600">Продаж фарб для дому та промисловості</p>
+  </div>
+</template>
