c61d7b594b7fb275fe2ceb179d8928e253a4090a
GeorgyPylyp <132196813+GeorgyPylyp@users.noreply.github.com>
2026-01-21 22:14:53 +0200

    Create Header.vue

---
+++ Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="bg-blue-600 text-white p-4">
+    <nav class="max-w-6xl mx-auto flex justify-between">
+      <h1 class="font-bold text-xl">Paint Shop</h1>
+      <div class="space-x-4">
+        <router-link to="/" class="hover:underline">Головна</router-link>
+        <router-link to="/products" class="hover:underline">Товари</router-link>
+        <router-link to="/cart" class="hover:underline">Кошик</router-link>
+      </div>
+    </nav>
+  </header>
+</template>
